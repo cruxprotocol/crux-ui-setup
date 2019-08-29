440efb7129cb908d1371d160e645d8075407c64d
mascot66993 <umang.shukla@coinswitch.co>
2019-08-29 13:42:43 +0000

    vueify the code

--- js/service.js
+++ js/service.js
@@ -1,27 +1,3 @@
-document.getElementById('retry-button').onclick = function(e){
-	// document.getElementById("error-message").classList.remove("loading");
-	document.getElementById("error-message").style.display = "none";
-	document.getElementById("overlay").style.display = "none";
-}
-
-
-document.getElementById('send-payment-request').onclick = function(e){
-	document.getElementById("error-message").classList.add("loading");
-	document.getElementById("error-message-text").innerHTML = "Please login into your OpenPay enabled app where you want to receive payment request.";
-	document.getElementById("error-message").style.display = "block";
-	document.getElementById("overlay").style.display = "block";
-}
-
-function transaction_failed() {
-	document.getElementById("error-message").classList.remove("loading");
-	document.getElementById("error-message-text").innerHTML = "Payment failed because OpenPay request expired.";
-}
-
-function transaction_channel_creation_acknowledged() {
-	document.getElementById("error-message-text").innerHTML = "Please accept the request from Uber on your OpenPay enabled app.";
-}
-
-function transaction_success() {
-	document.getElementById("error-message").classList.remove("loading");
-	document.getElementById("error-message-text").innerHTML = "Transaction confirmed";
+function test(){
+	console.log('test got printed!')
 }
