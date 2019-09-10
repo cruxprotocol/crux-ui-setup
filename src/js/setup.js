6bc89804a9342a9df9ea143b28eaff2c3bf328d3
Amit Singh <amit@coinswitch.co>
2019-09-10 18:54:25 +0530

    [UPDATE]: improving functions

--- src/js/setup.js
+++ src/js/setup.js
@@ -8,30 +8,47 @@ $(document).ready(function () {
         input: $('#cruxpayId'),
         inputParent: $('.cruxpay-id__container'),
         helperText: $('#cruxpay-id__helper-text'),
-        parentContainer: $('.cruxpay-id__container').parent()
-    }
-    
-    cruxpayId.input.on('keyup', (e) => {
-        if (e.target.value && e.target.value.length > 0) {
-            isUserIdAvailable(e.target.value)
+        parentContainer: $('.cruxpay-id__container').parent(),
+        displayError(message) {
+            this.helperText.removeClass('helper-text--success');
+            this.inputParent.addClass('mdc-text-field--invalid');
+            this.parentContainer.addClass('has-error');
+            this.helperText.html(message);
+        },
+        displaySuccess(message) {
+            cruxpayId.inputParent.removeClass('mdc-text-field--invalid');
+            cruxpayId.helperText.addClass('helper-text--success');
+            cruxpayId.parentContainer.removeClass('has-error');
+            cruxpayId.helperText.html(message);
+        },
+        displayHelpText(message) {
+            cruxpayId.helperText.removeClass('helper-text--success');
+            cruxpayId.inputParent.removeClass('mdc-text-field--invalid');
+            cruxpayId.parentContainer.removeClass('has-error');
+            cruxpayId.helperText.html(message);
         }
+    }
+
+    let timer = null;
+    cruxpayId.input.on('keyup', (e) => {
+        cruxpayId.displayHelpText(`Checking availability`);
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            if (e.target.value && e.target.value.length > 0) {
+                isUserIdAvailable(e.target.value)
+            }
+        }, 500)
     })
     function isUserIdAvailable(id) {
         $.ajax({
             type: "GET",
             url: `https://167.71.234.131:3000/status/${id}`,
             success: function (response) {
-                cruxpayId.helperText.removeClass('cruxpay-id__helper-text--success');
-                cruxpayId.inputParent.addClass('mdc-text-field--invalid');
-                cruxpayId.parentContainer.addClass('has-error');
-                cruxpayId.helperText.html(`${id} is unavailable`);
+                cruxpayId.displayError(`${id} is unavailable`);
             },
             error: function (xhr, ajaxOptions, thrownError) {
                 if (xhr.status == 404) {
-                    cruxpayId.helperText.addClass('cruxpay-id__helper-text--success');
-                    cruxpayId.inputParent.removeClass('mdc-text-field--invalid');
-                    cruxpayId.parentContainer.removeClass('has-error');
-                    cruxpayId.helperText.html(`${id} is available`);
+                    cruxpayId.displaySuccess(`${id} is available`);
                 }
             }
         });
